7be394108e984693ef8876ab34d4efd14ae713b3
Aram <aramsuqiasyan20@gmail.com>
2022-11-30 22:53:15 +0400

    modify authors section

--- components/Authors/index.tsx
+++ components/Authors/index.tsx
@@ -1,21 +1,26 @@
-import React, {useRef, useMemo, useEffect, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import gsap from "gsap";
 import Image from "next/image";
 import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
 
 import styles from './index.module.scss'
+import aramNFT from '../../assets/images/aramNFT.jpeg'
+import aramPicture from '../../assets/images/aramPicture.jpeg'
+import narekNFT from '../../assets/images/narekNFT.jpeg'
+import narekPicture from '../../assets/images/narekPicture.jpeg'
+import robNFT from '../../assets/images/robNFT.jpeg'
+import robPicture from '../../assets/images/robPicture.jpeg'
 import authorsImage from '../../assets/images/authors.jpeg'
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Authors = () => {
-    const [style, setStyle] = useState({x: 0, y: 0, translateX: -180})
-    const ref = useRef<HTMLDivElement>(null);
+    const [style, setStyle] = useState({x: 0, y: 0})
 
     const text = useMemo(() => {
-        const txt = 'We are running blockchain business for over 4 years. We set up IEOs, participated in NFT projects companies and collaborated with blockchain businesses & DeFi companies to build sustainable business models and grow community. We ran business development for most advanced anti-scam software, build a solution for biggest Swiss-based crypto broker, co-founded most popular marketing platform for crypto airdrops, and developed an advanced non-custodial multi-chain crypto wallet widely used in MENA region.'
+        const txt = '❝ We are running blockchain business for over 4 years. We set up IEOs, participated in NFT projects companies and collaborated with blockchain businesses & DeFi companies to build sustainable business models and grow community. We ran business development for most advanced anti-scam software, build a solution for biggest Swiss-based crypto broker, co-founded most popular marketing platform for crypto airdrops, and developed an advanced non-custodial multi-chain crypto wallet widely used in MENA region. ❞'
         return txt.split(' ').map((word, index) => {
-            return <span key={index} className={styles.glitch}>{word} </span>
+            return <span key={index} className={styles.glitch}><span>{word}</span> </span>
         })
     }, [])
 
@@ -23,18 +28,24 @@ const Authors = () => {
         return [
             {
                 id: 1,
-                title: 'Robert Mkrtchian 🫡',
+                title: 'Robert Mkrtchian',
                 profession: 'Product, Operations',
+                nft: robNFT,
+                picture: robPicture,
             },
             {
                 id: 2,
-                title: 'Narek Nadarian 🤑',
+                title: 'Narek Nadarian',
                 profession: 'Exo',
+                nft: narekNFT,
+                picture: narekPicture,
             },
             {
                 id: 3,
-                title: 'Aram Sukiasian 🤯',
+                title: 'Aram Sukiasian',
                 profession: 'Cit',
+                nft: aramNFT,
+                picture: aramPicture,
             },
         ]
     }, [])
@@ -45,7 +56,6 @@ const Authors = () => {
         setStyle({
             x: fromCenterX,
             y: fromCenterY,
-            translateX: 0
         })
     }
 
@@ -53,90 +63,54 @@ const Authors = () => {
         setStyle({
             x: 0,
             y: 0,
-            translateX: -180
         })
     }
 
-    useEffect(() => {
-        const element = ref.current;
-        if (element) {
-            let animation1 = gsap.fromTo(element.querySelectorAll(`.${styles.card}`),
-                {
-                    y: 200,
-                },
-                {
-                    y: 0,
-                    stagger: 0.1,
-                    scrollTrigger: {
-                        trigger: element,
-                        start: "top bottom-=100",
-                        end: "bottom bottom-=100",
-                        scrub: 0,
-                    }
-                }
-            );
-
-
-            let animation2 = gsap.fromTo(element.querySelectorAll(`.${styles.figures}`),
-                {
-                    top: 200,
-                    bottom: 200,
-                },
-                {
-                    top: -100,
-                    bottom: -100,
-                    scrollTrigger: {
-                        trigger: element,
-                        start: "top center",
-                        end: "bottom bottom",
-                        scrub: 2,
-                    }
-                }
-            );
-
-
-            return () => {
-                !!animation1 && animation1.kill();
-                !!animation2 && animation2.kill();
-            }
-        }
-    }, []);
-
 
     return (
-        <div ref={ref}
-             className={styles.sections}
-        >
+        <div className={styles.sections}>
             <div className={styles.sectionsInner}>
                 <div
                     className={styles.imageBox}
                     onMouseMove={onMouseMove}
                     onMouseOut={onMouseOut}
                     style={{
-                        transform: `translateX(${style.translateX}px) perspective(800px) rotateX(${style.y / 80}deg)  rotateY(${-style.x / 80}deg)`
+                        transform: `perspective(800px) rotateX(${style.y / 80}deg)  rotateY(${-style.x / 80}deg)`
                     }}
                 >
                     <Image src={authorsImage} alt={'image'}/>
                 </div>
-                <div className={styles.cards}>
-                    {data.map(item => {
-                        return (
-                            <div key={item.id}
-                                 className={styles.card}
-                            >
-                                <div className={styles.cardText}>
-                                    <h2 className={styles.cardTitle}>{item.title}</h2>
-                                    <p className={styles.cardDescription}>{item.profession}</p>
+                <div className={styles.mainBox}>
+                    <div className={styles.cards}>
+                        {data.map((item, index) => {
+                            return (
+                                <div key={item.id}
+                                     className={styles.card}
+                                >
+                                    <div className={styles.cardText}>
+                                        <div className={styles.glitchImages}>
+                                            <Image src={item.nft} alt={'nft'} className={styles.glitchImage}/>
+                                            <Image src={item.nft} alt={'nft'} className={styles.glitchImage}/>
+                                            <Image src={item.nft} alt={'nft'} className={styles.glitchImage}/>
+                                            <Image src={item.nft} alt={'nft'} className={styles.glitchImage}/>
+                                            <Image src={item.nft} alt={'nft'} className={styles.glitchImage}/>
+                                            <Image src={item.nft} alt={'nft'} className={styles.glitchImage}/>
+                                            <Image src={item.picture} alt={'nft'} className={styles.pictureImage}/>
+                                        </div>
+                                        <h2 className={styles.cardTitle}>{item.title}</h2>
+                                        <p className={styles.cardDescription}>{item.profession}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
+                    <p className={styles.sectionSubTitle}>
+                        {text}
+                        {text}
+                        {text}
+                    </p>
                 </div>
             </div>
-
-            <p className={styles.sectionSubTitle}>
-                {text}
-            </p>
         </div>
     )
 }
