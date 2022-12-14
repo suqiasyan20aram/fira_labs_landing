109ab87e5ec6412d29852c8df7119ac68066dc0e
Aram <aramsuqiasyan20@gmail.com>
2022-12-14 17:22:19 +0400

    add 404, building pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,11 @@
 import type {AppProps} from 'next/app'
-import {useState} from "react";
 
 import '../styles/globals.scss'
-import Menu from "../components/Menu";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function App({Component, pageProps}: AppProps) {
-    const [menuIsShown, setMenuIsShown] = useState(false);
-
-    const toggleMenu = (shown: boolean) => {
-        setMenuIsShown(shown)
-    }
-
     return (
         <div>
-            <Menu menuIsShown={menuIsShown} onClose={() => toggleMenu(false)}/>
-            <Header animated={true} menuIsShown={menuIsShown} toggleMenu={toggleMenu}/>
             <Component {...pageProps} />
-            <Footer/>
         </div>
     )
 
