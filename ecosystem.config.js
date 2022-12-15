770ec30472666664ab0139c002a105b5a38c1481
Aram <aramsuqiasyan20@gmail.com>
2022-12-15 16:49:48 +0400

    change configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       user : 'bitnami',
       host : '18.197.2.81',
       ref  : 'origin/master',
-      repo : 'git@github.com:suqiasyan20aram/fira_labs_landing.git',
+      repo : 'https://github.com/suqiasyan20aram/fira_labs_landing.git',
       path : 'home/bitnami',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && npm run build && npm install && pm2 reload ecosystem.config.js --env production',
