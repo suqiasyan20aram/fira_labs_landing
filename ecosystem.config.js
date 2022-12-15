9a84208ef30e503a2d6019a5ac13b9cd541fb586
Aram <aramsuqiasyan20@gmail.com>
2022-12-15 17:08:21 +0400

    pos deply

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'https://github.com/suqiasyan20aram/fira_labs_landing.git',
       path : 'home/bitnami',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm run build && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm run build && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
