71347f36d9d6545e091b6975c66c12e5d159d708
Aram <aramsuqiasyan20@gmail.com>
2022-12-16 11:56:33 +0400

    change repo with ssh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       user : 'bitnami',
       host : '35.157.98.153',
       ref  : 'origin/master',
-      repo : 'https://github.com/suqiasyan20aram/fira_labs_landing.git',
+      repo : 'git@github.com:suqiasyan20aram/fira_labs_landing.git',
       path : '',
       'pre-deploy-local': '',
       'post-deploy' : 'npm run build && npm install && pm2 reload ecosystem.config.js --env production',
