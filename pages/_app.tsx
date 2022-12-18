22ddcad95dce86c9d3e2f0c273b0334db4a8e24d
Aram <aramsuqiasyan20@gmail.com>
2022-12-18 19:31:49 +0400

    change title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type {AppProps} from 'next/app'
+import Head from "next/head";
 
 import '../styles/globals.scss'
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <div>
+            <Head>
+                <title>Decentures Ltd.</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
             <Component {...pageProps} />
         </div>
     )
